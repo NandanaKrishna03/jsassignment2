31896cd465837aa80e290a0422f63b0e921daa99
Nandanakrishna03 <nandanakrishna75@gmail.com>
2024-09-15 20:07:21 +0530

    index.html

---
+++ main.js
@@ -0,0 +1,14 @@
+function oddoreven(n){
+    if(n%2==0){
+        console.log("even");
+
+    }
+    else{
+        console.log("odd");
+        
+    }
+}
+oddoreven(21)
+oddoreven(11327468)
+oddoreven(1135781935)
+oddoreven(11334753482)
